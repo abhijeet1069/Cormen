1ec851bbdfca6f310fbd669d498ddbba64752444
abhijeet1069 <abhijeet1069@gmail.com>
2020-12-29 00:24:56 +0530

    merge sort working

--- src/introduction/MergeSort.java
+++ src/introduction/MergeSort.java
@@ -10,7 +10,7 @@ public class MergeSort {
 		for(int i = 0; i < n1; i++)
 			L[i] = arr[p+i];
 		for(int j = 0; j < n2; j++)
-			R[j] = arr[q+j];	
+			R[j] = arr[q+j+1];	
 		L[n1] = Integer.MAX_VALUE;
 		R[n2] = Integer.MAX_VALUE;
 		int i = 0, j = 0;
@@ -25,12 +25,22 @@ public class MergeSort {
 			}
 		}
 	}
+
+	public void mergeSort(int[] arr, int p, int r){
+		if(p < r){
+			int q = (p+r)/2;
+			mergeSort(arr,p,q);
+			mergeSort(arr,q+1,r);
+			merge(arr,p,q,r);
+		}
+		
+	}
 	
 	public static void main(String[] args) {
 		int[] arr = new int[]{12,3,-2,3,4,23,42};
 		int p = 0, r = arr.length-1;
 		MergeSort ms = new MergeSort();
-		ms.merge(arr, p, (p+r)/2, r);
+		ms.mergeSort(arr,p,r);
 		for(int el : arr)
 			System.out.print(el+" ");
 	}
