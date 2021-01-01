987eb07e3a0258d255683c9f0f11d1a009b4a8bf
abhijeet1069 <abhijeet1069@gmail.com>
2021-01-01 10:51:30 +0530

    bin deleted

---
+++ src/miscellaneous/demo.java
@@ -0,0 +1,10 @@
+package miscellaneous;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int i = 32;
+        System.out.println("Value of i = "+i);
+
+    }
+}
